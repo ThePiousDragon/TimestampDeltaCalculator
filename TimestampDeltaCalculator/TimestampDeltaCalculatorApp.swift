2ba739f2d249175b6b97c8878564abb9d4c6c444
ThePiousDragon <auspice-foamier-0f@icloud.com>
2022-12-21 11:20:10 +0000

    Initial Commit

---
+++ TimestampDeltaCalculator/TimestampDeltaCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimestampDeltaCalculatorApp.swift
+//  TimestampDeltaCalculator
+//
+//  Created by dimitar on 21/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct TimestampDeltaCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
